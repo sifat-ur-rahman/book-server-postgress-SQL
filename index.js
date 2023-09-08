1da37326c349d481ce6c16963f5b720e16c91c64
sifat <md.sifat.ur.rahman2702@gmail.com>
2023-09-08 08:06:08 +0600

    add

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 const express = require('express');
 
-
+const PORT = 5000
 const app = express()
+
+
+
+
+app.listen(PORT, ()=>{
+    console.log(`postgress SQL Book server is running at ${PORT}`)
+
+})
